3e0ed41a142e417e44d78907c9760cfb1180f0eb
Suyash1 Tiwari <Suyash1.Tiwari@ril.com>
2024-03-10 16:17:55 +0530

    server-side code deployment

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import userRouter from "./routes/users.js";
+import taskRouter from './routes/task.js'
+import { config } from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { errorMiddleware } from './middlewares/error.js';
+import cors from 'cors';
+
+export const app = express();
+
+config({
+    path: "./data/config.env",
+});
+
+//using middleware's
+app.use(express.json());
+app.use(cookieParser());
+//using routes
+app.use("/api/v1/users", userRouter);  // /users we are creating prefix
+app.use("/api/v1/task", taskRouter);
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+app.get("/", (req,res) => {
+   res.send("Nice Working");
+});
+
+app.use(errorMiddleware);   //error middleware
