2205d285e89f9e84b1ba3a533f3f6a02cdc80265
Suyash1 Tiwari <Suyash1.Tiwari@ril.com>
2024-03-15 22:23:24 +0530

    changes for cors error 7

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ app.use("/api/v1/task", taskRouter);
 
 
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+// app.use(cors(corsOptions)) // Use this after the variable declaration
 
 
 app.get("/", (req,res) => {
