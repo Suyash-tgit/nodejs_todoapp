95f39d3ad6531f91b851426ae2b91f88c59eec92
Suyash1 Tiwari <Suyash1.Tiwari@ril.com>
2024-03-15 19:00:34 +0530

    changes for cors error 4

--- app.js
+++ app.js
@@ -12,24 +12,33 @@ config({
     path: "./data/config.env",
 });
 
+const corsWithOptions = (req, res, next) => {
+    const allowedOrigins = [process.env.FRONTEND_URL]; // List of allowed origins
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+};
+
+
 //using middleware's
 app.use(express.json());
 app.use(cookieParser());
 //using routes
 app.use("/api/v1/users", userRouter);  // /users we are creating prefix
 app.use("/api/v1/task", taskRouter);
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+// }));
 
-// Middleware to enable CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+// Enable CORS with dynamic origin setting
+app.use(corsWithOptions);
 
 app.get("/", (req,res) => {
    res.send("Nice Working");
